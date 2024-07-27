9f1225ef8e90a886a5200bbfe49d47a953af8a8f
Abhi-jaiswal <abhishek.jaiswalln11a@gmail.com>
2024-07-27 22:17:05 +0530

    update order

--- backend/controller/user/updateMyOrders.js
+++ backend/controller/user/updateMyOrders.js
@@ -2,45 +2,57 @@ const userSchema = require("../../models/userModel");
 const Profile = require("../../models/Profile");
 
 const updateMyOrders = async (req, res) => {
-    const userId = req.userId;
-    const { products } = req.body;
+  const userId = req.userId;
+  const { products } = req.body;
 
-    try {
-        const user = await userSchema.findById(userId);
+  try {
+    const user = await userSchema.findById(userId);
 
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        let profile;
-
-        if (user.additionalDetails) {
-            // If additionalDetails exists, find the corresponding Profile
-            profile = await Profile.findById(user.additionalDetails);
-            if (!profile) {
-                return res.status(400).json({
-                    message: "Profile associated with user not found",
-                    error: true,
-                    success: false,
-                });
-            }
-        } 
-        if (!profile) {
-            return res.status(404).json({ message: "Profile not found" });
-        }
-
-        products.forEach(product => {
-            profile.myOrders.push({
-                productId: product.productId,
-                quantity: product.quantity
-            });
-        });
-
-        await profile.save();
-        res.status(200).json(profile);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    let profile;
+
+    // Check if additionalDetails exists
+    if (user.additionalDetails) {
+      // If additionalDetails exists, find the corresponding Profile
+      profile = await Profile.findById(user.additionalDetails);
+      if (!profile) {
+        return res.status(400).json({
+          message: "Profile associated with user not found",
+          error: true,
+          success: false,
+        });
+      }
+    } else {
+      // If additionalDetails does not exist, create a new Profile
+      profile = new Profile({
+        gender: "",
+        dateOfBirth : "",
+        address : "",
+        contactNumber   : "",
+      });
+      await profile.save();
+
+      // Associate the new Profile with the user
+      user.additionalDetails = profile._id;
+    }
+
+    profile = await Profile.findById(user.additionalDetails);
+
+    products.forEach((product) => {
+      profile.myOrders.push({
+        productId: product._id,
+        quantity: product.quantity,
+      });
+    });
+
+    await profile.save();
+    res.status(200).json(profile);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = { updateMyOrders };
