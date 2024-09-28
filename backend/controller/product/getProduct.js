c557bc010df8917a5cd14d58dcffa2e6120d567d
Harshsaini7 <hk3735594@gmail.com>
2024-09-28 23:41:33 +0530

    hui

--- backend/controller/product/getProduct.js
+++ backend/controller/product/getProduct.js
@@ -1,26 +1,24 @@
-const productModel = require("../../models/productModel");
+const productModel = require("../../models/productModel")
 
-const getProductController = async (req, res) => {
-  try {
-    // Find all products and populate the user field in each review
-    const allProduct = await productModel.find()
-      .sort({ createdAt: -1 })
-      .populate('reviews.user', 'firstName lastName email'); // Populate user fields (firstName, lastName, email)
+const getProductController = async(req,res)=>{
+    try{
+        const allProduct = await productModel.find().sort({ createdAt : -1 })
 
-    res.json({
-      message: "All Product",
-      success: true,
-      error: false,
-      data: allProduct
-    });
+        res.json({
+            message : "All Product",
+            success : true,
+            error : false,
+            data : allProduct
+        })
 
-  } catch (err) {
-    res.status(400).json({
-      message: err.message || err,
-      error: true,
-      success: false
-    });
-  }
-};
+    }catch(err){
+        res.status(400).json({
+            message : err.message || err,
+            error : true,
+            success : false
+        })
+    }
 
-module.exports = getProductController;
+}
+
+module.exports = getProductController
